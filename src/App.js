53f81c9c9dedc5644304e78d35de69854b7b9fd4
mathan1995 <mathanganjeya@gmail.com>
2019-05-29 02:55:23 +0530

    Added Some Changes In Routing For all Dashboard Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-// import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
-// import QALayout from './components/Company/QA/QALayout'
+import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
+// import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
-import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
-// import PMLayout from './components/Company/PM/PMLayout'
+// import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
+// import PMLayout from "./components/Company/PM/PMLayout";
 
 function App() {
   return (
     <div>
       {/* <QALayout /> */}
       {/* <DeveloperLayout /> */}
-      <ProductAdminLayout />
-      {/* <CompanyLayout /> */}
-      {/* <PMLayout />   */}
+      {/* <ProductAdminLayout /> */}
+      <CompanyLayout />
+      {/* <PMLayout /> */}
     </div>
   );
 }
