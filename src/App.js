d5bd11f173a75de15c2cd02965c55e714bebb830
asriel94 <thenu38@gmail.com>
2019-05-29 19:34:05 +1000

    added login form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
+// import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
 // import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
 // import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
@@ -8,6 +8,7 @@ import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
 // import PMLayout from "./components/Company/PM/PMLayout";
 // import CommonLayout from './components/Company/CommonDashboards/CommonLayout'
 // import Main from "./components/MainPanel/Main";
+import Login from "./components/User/Login";
 
 function App() {
   return (
@@ -17,10 +18,11 @@ function App() {
       {/* <ProductAdminLayout /> */}
       {/* <CompanyLayout /> */}
       {/* <PMLayout />   */}
-      <CompanyLayout />
+      {/* <CompanyLayout /> */}
       {/* <PMLayout /> */}
       {/* <CommonLayout /> */}
       {/* <Main /> */}
+      <Login />
     </div>
   );
 }
