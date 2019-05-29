5e591161ffef91fc822c949573089586e873a615
mathan1995 <mathanganjeya@gmail.com>
2019-05-29 22:08:57 +0530

    Waving the changes from >> Dilux

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import "antd/dist/antd.css";
 // import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
 // import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
-// import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
+import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
 // import PMLayout from "./components/Company/PM/PMLayout";
 // import CommonLayout from "./components/Company/CommonDashboards/CommonLayout";
 // import Main from "./components/MainPanel/Main";
-import Login from "./components/User/Login";
+// import Login from "./components/User/Login";
 
 function App() {
   return (
     <div>
       {/* <QALayout /> */}
       {/* <DeveloperLayout /> */}
-      {/* <ProductAdminLayout /> */}
+      <ProductAdminLayout />
       {/* <PMLayout /> */}
       {/* <CompanyLayout /> */}
       {/* <CommonLayout /> */}
       {/* <Main /> */}
       {/* <CommonLayout /> */}
-      <Login />
+      {/* <Login /> */}
     </div>
   );
 }
