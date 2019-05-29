dd64efbe5165ccab92317e7fe1c5e8f28de1e178
mathan1995 <mathanganjeya@gmail.com>
2019-05-29 21:11:42 +0530

    Waving For Tyrone >> Push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "antd/dist/antd.css";
-import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
+// import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
 // import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
 // import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
-// import PMLayout from './components/Company/PM/PMLayout'
+import PMLayout from "./components/Company/PM/PMLayout";
 // import PMLayout from "./components/Company/PM/PMLayout";
 // import CommonLayout from './components/Company/CommonDashboards/CommonLayout'
 // import Main from "./components/MainPanel/Main";
@@ -16,8 +16,8 @@ function App() {
       {/* <DeveloperLayout /> */}
       {/* <ProductAdminLayout /> */}
       {/* <CompanyLayout /> */}
-      {/* <PMLayout />   */}
-      <CompanyLayout />
+      <PMLayout />
+      {/* <CompanyLayout /> */}
       {/* <PMLayout /> */}
       {/* <CommonLayout /> */}
       {/* <Main /> */}
