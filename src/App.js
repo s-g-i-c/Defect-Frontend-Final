97e1c0bc72654c3600804406e80f0b208538d0ed
mathan1995 <mathanganjeya@gmail.com>
2019-05-30 03:07:57 +0530

    Added Profie Changes And Routing for All

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "antd/dist/antd.css";
 // import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
-import QALayout from "./components/Company/QA/QALayout";
+// import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
 // import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
 // import PMLayout from './components/Company/PM/PMLayout'
@@ -10,13 +10,13 @@ import QALayout from "./components/Company/QA/QALayout";
 // import PMLayout from "./components/Company/PM/PMLayout";
 // import CommonLayout from "./components/Company/CommonDashboards/CommonLayout";
 // import Main from "./components/MainPanel/Main";
-// import Main from "./components/MainPanel/Main";
+import Main from "./components/MainPanel/Main";
 // import Login from "./components/User/Login";
 
 function App() {
   return (
     <div>
-      <QALayout />
+      {/* <QALayout /> */}
       {/* <DeveloperLayout /> */}
       {/* <ProductAdminLayout /> */}
       {/* <CompanyLayout /> */}
