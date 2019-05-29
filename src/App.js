8441df64155bd019a5bb468f609e59b90a631ff5
dilanMD <deluxan.sgic@gmail.com>
2019-05-30 03:06:30 +0530

    Developer Dashboard => Altered from previous design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "antd/dist/antd.css";
 // import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
-import QALayout from "./components/Company/QA/QALayout";
-// import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
+// import QALayout from "./components/Company/QA/QALayout";
+import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
 // import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
 // import PMLayout from './components/Company/PM/PMLayout'
 // import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
@@ -16,8 +16,8 @@ import QALayout from "./components/Company/QA/QALayout";
 function App() {
   return (
     <div>
-      <QALayout />
-      {/* <DeveloperLayout /> */}
+      {/* <QALayout /> */}
+      <DeveloperLayout />
       {/* <ProductAdminLayout /> */}
       {/* <CompanyLayout /> */}
       {/* <PMLayout /> */}
