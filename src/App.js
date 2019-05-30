82c74489aa417b7da01cf2501513bff546d09e1a
Tyron <tyronne90@gmail.com>
2019-05-30 09:45:43 +0530

    pull req

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "antd/dist/antd.css";
-// import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
+import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
 // import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
-import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
+// import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
 // import PMLayout from "./components/Company/PM/PMLayout";
 // import CommonLayout from "./components/Company/CommonDashboards/CommonLayout";
-// import Main from "./components/MainPanel/Main";
-// import CommonLayout from './components/Company/CommonDashboards/CommonLayout'
 // import Main from "./components/MainPanel/Main";
 // import Login from "./components/User/Login";
 
@@ -16,13 +14,12 @@ function App() {
     <div>
       {/* <QALayout /> */}
       {/* <DeveloperLayout /> */}
-      <ProductAdminLayout />
-      {/* <CompanyLayout /> */}
+      {/* <ProductAdminLayout /> */}
+      <CompanyLayout />
       {/* <PMLayout /> */}
       {/* <CompanyLayout /> */}
       {/* <CommonLayout />  */}
       {/* <Main /> */}
-      {/* <CommonLayout /> */}
       {/* <Main /> */}
       {/* <Login /> */}
     </div>
