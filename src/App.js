95684cf982bc959156f6753ebe34df1964163fef
Tyron <tyronne90@gmail.com>
2019-05-30 14:35:28 +0530

    corrected config button in QA dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "antd/dist/antd.css";
 // import CompanyLayout from "./components/Company/CompanyAdmin/CompanyLayout";
-// import QALayout from "./components/Company/QA/QALayout";
+import QALayout from "./components/Company/QA/QALayout";
 // import DeveloperLayout from "./components/Company/Developer/DeveloperLayout";
 // import ProductAdminLayout from "./components/ProductAdmin/ProductAdminLayout";
 // import PMLayout from "./components/Company/PM/PMLayout";
 // import CommonLayout from "./components/Company/CommonDashboards/CommonLayout";
-import Main from "./components/MainPanel/Main";
+// import Main from "./components/MainPanel/Main";
 // import Login from "./components/User/Login";
 // import Routes from "./components/User/Routes";
 
-// import ManDef from "./components/Company/PM/Allocation";
+// import ManDef from "./components/Company/QA/Dashboard";
 
 // import QAPrivilage from "./components/Company/QA/Privillages";
 // import PAPrivilage from "./components/ProductAdmin/Privillages";
@@ -19,12 +19,12 @@ function App() {
   return (
     <div>
       {/* <CompanyLayout /> */}
-      {/* <QALayout /> */}
+      <QALayout />
       {/* <DeveloperLayout /> */}
       {/* <ProductAdminLayout /> */}
       {/* <PMLayout /> */}
       {/* <CommonLayout /> */}
-      <Main />
+      {/* <Main /> */}
       {/* <Login /> */}
       {/* <Routes />
 
